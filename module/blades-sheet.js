93984a00c81dc44645b826eda2ba1eb12236163c
Dez384 <164457313+Dez384@users.noreply.github.com>
2025-01-11 23:07:14 -0500

    Data Consolidation
    - moved files to correct directories
    - deduplicated listeners and related function by moving them to blades-sheet.js
    - opening a contact that is not imported to the world will now import the actor and connect the imported actor to the character sheet

--- module/blades-sheet.js
+++ module/blades-sheet.js
@@ -12,6 +12,10 @@ export class BladesSheet extends ActorSheet {
     super.activateListeners(html);
     html.find(".item-add-popup").click(this._onItemAddClick.bind(this));
     html.find(".update-box").click(this._onUpdateBoxClick.bind(this));
+	html.find("input.radio-toggle, label.radio-toggle").click((e) => e.preventDefault());
+	html.find("input.radio-toggle, label.radio-toggle").mousedown((e) => {
+		this._onRadioToggle(e);
+    });
 
     // Post item to chat
     html.find(".item-post").click((ev) => {
@@ -26,6 +30,106 @@ export class BladesSheet extends ActorSheet {
     }
 
     html.find(".roll-die-attribute").click(this._onRollAttributeDieClick.bind(this));
+	
+		// acquaintance status toggle
+    html.find('.standing-toggle').click(ev => {
+      let acquaintances = this.actor.system.acquaintances;
+      let acqId = ev.target.closest('.acquaintance').dataset.acquaintance;
+      let clickedAcqIdx = acquaintances.findIndex(item => item.id == acqId);
+      let clickedAcq = acquaintances[clickedAcqIdx];
+      let oldStanding = clickedAcq.standing;
+      let newStanding;
+      switch(oldStanding){
+        case "friend":
+          newStanding = "rival";
+          break;
+        case "rival":
+          newStanding = "neutral";
+          break;
+        case "neutral":
+          newStanding = "friend";
+          break;
+      }
+      clickedAcq.standing = newStanding;
+      acquaintances.splice(clickedAcqIdx, 1, clickedAcq);
+      this.actor.update({system: {acquaintances : acquaintances}});
+    });
+	
+	  // Open Acquaintance
+    html.find('.open-friend').click(ev => {
+		//game.actors.importFromCompendium(game.packs.get("blades-in-the-dark.npc"),"Dv29btX5yhOkX8Pu")
+      const element = $(ev.currentTarget).parents(".item");
+		//acqId is the UUID of the Acquaintance
+	  let acqId = element.data("itemId");
+		// if the Acquaintance is not in the world the if loop will trigger
+	  if (game.actors.get(element.data("itemId")) == undefined) {
+		  //send the UUID and this actor to a helper fuction
+		  BladesHelpers.importAcquaintance(this.actor, acqId);
+	  } else {
+      const actor = game.actors.get(element.data("itemId"));
+      actor?.sheet.render(true);
+	  }
+    });
+	
+	// Remove Acquaintance from character sheet
+    html.find('.acquaintance-delete').click(ev => {
+      //let acqId = ev.target.closest('.acquaintance').dataset.acquaintance; //used when <div class="acquaintance"
+	  const element = $(ev.currentTarget).parents(".item");
+	  let acqId = element.data("itemId");
+	  BladesHelpers.removeAcquaintance(this.actor, acqId);
+    });
+
+	  // Import Acquaintance by playbook
+    html.find('.import-contacts').click(ev => {
+	  const actor_type = this.actor.type;
+	  let item_type;
+	  if (actor_type=="character") {item_type = "class";}
+		else if (actor_type=="crew") {item_type = "crew_type";}
+	  const playbook = this.actor.items.filter(i=> i.type === item_type)[0]?.name;
+	  BladesHelpers.import_pb_contacts(this.actor, playbook);
+
+    });
+
+		// Increment Exp Clock
+	html.find('.up-exp-clock').click(ev => {
+		let value = this.actor.system.exp_clock.value;
+		let number = this.actor.system.exp_clock.number;
+		value = value + 1;
+		if (value >= this.actor.system.exp_clock.size) {
+			value = 0;
+			number = number + 1;
+		}
+		this.actor.update({"system.exp_clock": {value : value, number : number}});
+	});
+	
+			// Decrement Exp Clock
+	html.find('.down-exp-clock').click(ev => {
+		let value = this.actor.system.exp_clock.value;
+		let number = this.actor.system.exp_clock.number;
+		value = value - 1;
+		if (value < 0) {
+			value = this.actor.system.exp_clock.size - 1;
+			number = number - 1;
+		}
+		this.actor.update({"system.exp_clock": {value : value, number : number}});
+	});
+	
+			// Add a whole Exp Clock
+	html.find('.add-exp-clock').click(ev => {
+		let number = this.actor.system.exp_clock.number;
+		number = number + 1;
+		this.actor.update({"system.exp_clock": {number : number}});
+	});
+	
+				// Remove a whole Exp Clock
+	html.find('.minus-exp-clock').click(ev => {
+		let number = this.actor.system.exp_clock.number;
+		if (number > 0) {number = number - 1;}
+		else {number = 0;}
+		this.actor.update({"system.exp_clock": {number : number}});
+	});
+	
+	
   }
 
   /* -------------------------------------------- */
@@ -144,5 +248,25 @@ export class BladesSheet extends ActorSheet {
     }
 
   /* -------------------------------------------- */
+  
+   async _onRadioToggle(event) {
+    let type = event.target.tagName.toLowerCase();
+    let target = event.target;
+    if (type == "label") {
+      let labelID = $(target).attr("for");
+      target = $(`#${labelID}`).get(0);
+    }
+    if (target.checked) {
+      //find the next lowest-value input with the same name and click that one instead
+      let name = target.name;
+      let value = parseInt(target.value) - 1;
+      this.element
+        .find(`input[name="${name}"][value="${value}"]`)
+        .trigger("click");
+    } else {
+      //trigger the click on this one
+      $(target).trigger("click");
+    }
+  }	
 
 }
